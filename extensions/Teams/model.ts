6ba9ac0517607c42d1d667134185107f3a266b7c
toughmuffin12 <81653049+toughmuffin12@users.noreply.github.com>
2024-06-13 18:01:42 +0000

    Updated teams integration

---
+++ extensions/Teams/model.ts
@@ -0,0 +1,59 @@
+export type AskResponse = {
+    conversation_id: string;
+    answer: string;
+    citations: Citation[];
+    error?: string;
+};
+
+export type Citation = {
+    content: string;
+    id: string;
+    title: string | null;
+    filepath: string | null;
+    url: string | null;
+    sharepoint_link: string | null;
+    metadata: string | null;
+    chunk_id: string | null;
+    reindex_id: string | null;
+}
+
+export enum CardType {
+    OpenUrl = "Action.OpenUrl",
+    ShowCard = "Action.ShowCard",
+    AdaptiveCard = "AdaptiveCard",
+    TextBlock = "TextBlock"
+}
+
+export type ToolMessageContent = {
+    citations: Citation[];
+    intent: string;
+}
+
+export type ChatMessage = {
+    role: string;
+    content: string;
+    end_turn?: boolean;
+};
+
+export enum ChatCompletionType {
+    ChatCompletion = "chat.completion",
+    ChatCompletionChunk = "chat.completion.chunk"
+}
+
+export type ChatResponseChoice = {
+    messages: ChatMessage[];
+}
+
+export type ChatResponse = {
+    id: string;
+    model: string;
+    created: number;
+    object: ChatCompletionType;
+    choices: ChatResponseChoice[];
+    error: string;
+}
+
+export type ConversationRequest = {
+    id?: string;
+    messages: ChatMessage[];
+};
